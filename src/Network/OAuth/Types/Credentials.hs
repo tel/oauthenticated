9d718244a101ba161b6593c3118321837937cb1b
Dan Fithian <dan.fithian@tvisioninsights.com>
2018-02-22 11:41:28 -0500

    Push the GHC options into the stack invocation

--- src/Network/OAuth/Types/Credentials.hs
+++ src/Network/OAuth/Types/Credentials.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-redundant-constraints -fno-warn-incomplete-patterns #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings  #-}
 
